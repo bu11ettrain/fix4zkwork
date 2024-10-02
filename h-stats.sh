0e1a810358221f8869605db977bb33a380ed8a0b
bu11ettrain <30414204+bu11ettrain@users.noreply.github.com>
2024-10-02 16:05:39 +0500

    Update h-stats.sh

--- h-stats.sh
+++ h-stats.sh
@@ -2,14 +2,20 @@
 
 source /hive/miners/custom/aleo_prover/aleo_prover.conf
 
+logPart=`tail -n 50 ${CUSTOM_LOG_BASENAME}.log`
+
 #################################################################################################################################################
-#It should be fixed inside miner (you just need to implement correct exception handling and stop it instead of send error message continously). #
-#Since miner is not open sourced it's all what I can do to prevent RAM\HDD overflowing and freezeing.											#
-#But this small fix does its job pretty well though. :)																							#
+#It should be fixed inside miner (you just need to implement correct an exception handling and stop miner instead of send error message continously). 
+#Since miner is not open sourced it's all what I can do to prevent RAM\HDD overflowing and freezeing.											
+#This small fix does its job pretty well though. :)																							
 #################################################################################################################################################
-[[ -n $(tail -n 10 "${CUSTOM_LOG_BASENAME}.log" | grep "an illegal memory access was encountered") ]] && \										#
-rm -f /run/hive/miner.1 && \																													#
-/hive/bin/miner restart && \																													#
+#[[ -n $(tail -n 10 "${CUSTOM_LOG_BASENAME}.log" | grep "an illegal memory access was encountered") ]] && \		
+[[ -n $(echo "$logPart" | grep "an illegal memory access was encountered") ]] && \
+rm -f /run/hive/miner.1 && \
+/hive/bin/miner restart && \
+#This part is optional. It just saves BUSID of faulted GPUs to the logfile for further OC adjusting. 
+echo "$(date +"%Y-%m-%d %H:%M:%S") GPU FAULT" >> /home/user/gpu_fault.log && \
+dmesg -T | grep "Xid" | tail -n 1 >> /home/user/gpu_fault.log
 #################################################################################################################################################
 
 gpu_stats=$(< $GPU_STATS_JSON)
@@ -24,10 +30,8 @@ busid_arr=()
 fan_arr=()
 temp_arr=()
 lines=()
-index_arr=()
 
-logPart=`tail -n 50 ${CUSTOM_LOG_BASENAME}.log | sed -n '/gpu\[0\]/,/gpu\[\*\]/p' | head -n -1 | tac | awk '/gpu\[\*]/{exit} {print}' | tac`
-
+miner_stats=`echo "$logPart" | sed -n '/gpu\[0\]/,/gpu\[\*\]/p' | head -n -1 | tac | awk '/gpu\[\*]/{exit} {print}' | tac`
 while read -r string; do
 	[[ ! `echo $string | grep 'gpu'` ]] && continue
 	gpu_index=`echo $string | awk -F'[][]' '{print $2}'`
@@ -35,10 +39,9 @@ while read -r string; do
 	[[ "${busids[$gpu_index]}" =~ ^([A-Fa-f0-9]+): ]]
 	busid_arr+=($((16#${BASH_REMATCH[1]})))
 	hash_arr+=($gpu_hr)
-	index_arr+=($gpu_index)
 	temp_arr+=(${temps[$gpu_index]})
 	fan_arr+=(${fans[$gpu_index]})	
-done <<< $logPart    
+done <<< $miner_stats    
 
 hash_json=`printf '%s\n' "${hash_arr[@]}" | jq -cs '.'`
 bus_numbers=`printf '%s\n' "${busid_arr[@]}"  | jq -cs '.'`
@@ -47,7 +50,7 @@ temp_json=`printf '%s\n' "${temp_arr[@]}"  | jq -cs '.'`
 uptime=$(( `date +%s` - `stat -c %Y $CUSTOM_CONFIG_FILENAME` ))
 ac=$(grep -i 'found a solution' "${CUSTOM_LOG_BASENAME}.log" | wc -l)
 rj=0
-total_hashrate=$(tail -n 50 ${CUSTOM_LOG_BASENAME}.log | grep -F "gpu[*]:" | tail -n 1 | awk '{print $5}')
+total_hashrate=$(echo "$logPart" | grep -F "gpu[*]:" | tail -n 1 | awk '{print $5}')
 hs_units="hs"
 khs=$(echo "scale=5; $total_hashrate/1000" | bc)
 
@@ -56,16 +59,16 @@ stats=$(jq -nc \
 	--arg total_khs "$khs" \
 	--arg khs "$khs" \
 	--arg hs_units "$hs_units" \
-    --argjson hs "$hash_json" \
-    --arg ver "$CUSTOM_VERSION" \
-    --arg ac "$ac" \
-    --arg rj "$rj" \
-    --argjson bus_numbers "$bus_numbers" \
-    --argjson fan "$fan_json" \
-    --argjson temp "$temp_json" \
-    --arg uptime "$uptime" \
-    --arg algo "${CUSTOM_ALGO}" \
-    '{$total_khs, $khs, $hs_units, $hs, $temp, $fan, $uptime, $ver, ar: [$ac, $rj], $algo, $bus_numbers}')
+	--argjson hs "$hash_json" \
+	--arg ver "$CUSTOM_VERSION" \
+	--arg ac "$ac" \
+	--arg rj "$rj" \
+	--argjson bus_numbers "$bus_numbers" \
+	--argjson fan "$fan_json" \
+	--argjson temp "$temp_json" \
+	--arg uptime "$uptime" \
+	--arg algo "${CUSTOM_ALGO}" \
+	'{$total_khs, $khs, $hs_units, $hs, $temp, $fan, $uptime, $ver, ar: [$ac, $rj], $algo, $bus_numbers}')
 
 [[ -z $khs ]] && khs=0
 [[ -z $stats ]] && stats="null"
