6664b571f46ca038b53d11ccb8b7c6b2fa57252b
bu11ettrain <30414204+bu11ettrain@users.noreply.github.com>
2024-10-04 16:45:29 +0500

    Update h-run.sh

--- h-run.sh
+++ h-run.sh
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash    
+#!/usr/bin/env bash
 
 [[ `ps aux | grep "aleo_prover --address" | grep -v grep | wc -l` != 0 ]] && echo  "/hive/miners/custom/aleo_prover is already running" && exit 1
 
-cd `dirname $0`  
+cd `dirname $0`
 
-[ -t 1 ] && . colors                                                                                                                                                                            
+[ -t 1 ] && . colors
 
-. aleo_prover.conf                                                                                                                                                                            
+. aleo_prover.conf
 
-echo "ADDRESS = "$ADDRESS                                                                                                                                                                     
-echo "POOL = "$POOL                                                                                                                                                                           
-echo "WORKER_NAME = "$WORKER_NAME                                                                                                                                                             
+echo "ADDRESS = "$ADDRESS
+echo "POOL = "$POOL
+echo "WORKER_NAME = "$WORKER_NAME
 echo "CUSTOM_USER_CONFIG = "$CUSTOM_USER_CONFIG
 
 echo "CUSTOM_NAME = "$CUSTOM_NAME
@@ -21,14 +21,19 @@ echo "CUSTOM_CONFIG_FILENAME = "$CUSTOM_CONFIG_FILENAME
 
 [[ -z $CUSTOM_LOG_BASENAME ]] && echo -e "${RED}No CUSTOM_LOG_BASENAME is set${NOCOLOR}" && exit 1
 [[ -z $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}No CUSTOM_CONFIG_FILENAME is set${NOCOLOR}" && exit 1
-[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1  
+[[ ! -f $CUSTOM_CONFIG_FILENAME ]] && echo -e "${RED}Custom config ${YELLOW}$CUSTOM_CONFIG_FILENAME${RED} is not found${NOCOLOR}" && exit 1
 
 
 CUSTOM_LOG_BASEDIR="$CUSTOM_LOG_BASENAME/"
 [[ ! -d $CUSTOM_LOG_BASEDIR ]] && mkdir -p $CUSTOM_LOG_BASEDIR
 
-                                                                                                                                 
-if gpu-detect list; then                                                                                                                                                                  
-    echo "GPU Detected"                                                                                                                                                                   
-    ./aleo_prover --address $ADDRESS --pool $POOL --custom_name $WORKER_NAME $CUSTOM_USER_CONFIG 2>&1 | tee ${CUSTOM_LOG_BASENAME}.log                                                                                                       
-fi                                                                                                                                                                                        
+POOLS=""
+
+for i in $POOL; do
+    POOLS+="--pool $i "
+done
+
+if gpu-detect list; then
+    echo "GPU Detected"
+    ./aleo_prover --address $ADDRESS $POOLS --custom_name $WORKER_NAME $CUSTOM_USER_CONFIG 2>&1 | tee ${CUSTOM_LOG_BASENAME}.log
+fi
